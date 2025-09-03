74cebb300b4199e147031cbbda8e88ad7d9fbe36
Kyle Berkson <kyleberkson@gmail.com>
2025-09-02 17:57:10 -0700

    rename notebook to tutorial 7

--- build/lib/bhnerf/__init__.py
+++
@@ -1,8 +0,0 @@
-import warnings
-
-from . import utils, emission, visualization, network, optimization, constants, kgeo, alma
-
-try:
-    from . import observation
-except ImportError:
-    warnings.warn("observation.py not imported. To use EHT observation install eht-imaging: https://github.com/achael/eht-imaging")
